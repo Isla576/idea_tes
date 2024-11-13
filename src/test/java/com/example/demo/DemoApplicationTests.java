dca0b26b159626a082782c26e03473716f13650e
islaLiu <3179978785@qq.com>
2024-11-13 21:41:22 +0800

    update005

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -8,7 +8,7 @@ class DemoApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println("update");
+        System.out.println("update0005");
         System.out.println("second");
         System.out.println("first");
 
